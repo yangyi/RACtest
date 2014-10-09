d852a15168de60d1db70d5ebc4c9cf23c2c303d8
Joe Yang <joe.yangyi@gmail.com>
2014-10-09 19:39:07 +0800

    finite signal without capturing self
    The code use [1,2,3,4] sequence signal here for an finite signal, but
    it the rule should also apply network request and other finite signals
    
    The initial code is here:
    
    	class SignalViewController: UIViewController {
    
    	    weak var signal: RACSignal! // use weak var so taht
    `SignalViewController` will not retain the signal
    
    	    override func awakeFromNib() {
    	        signal = ([1,2,3,4] as NSArray).rac_sequence.signal()
    	        signal.subscribeNext { (e) in
    	            println("number \(e)")
    	        }
    	    }
    
    	    deinit {
    	        println("deinit SignalViewController, and signal is \(signal)")
    	    }
    	}
    
    To test the result, nothing special here
    
    1. run the app with current commit
    2. push the signal view controller, and it print the signal 1,2,3,4
    3. pop the signal view controller, view controller get deallocated, and
    echo the message, and found the signal is already deallocated

--- RACtest/SignalViewController.swift
+++ RACtest/SignalViewController.swift
@@ -10,26 +10,18 @@ import UIKit
 
 class SignalViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    weak var signal: RACSignal! // use weak var so taht `SignalViewController` will not retain the signal
+    
+    override func awakeFromNib() {
+        signal = ([1,2,3,4] as NSArray).rac_sequence.signal()
+        signal.subscribeNext { (e) in
+            println("number \(e)")
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    deinit {
+        println("deinit SignalViewController, and signal is \(signal)")
     }
-    */
+
 
 }
