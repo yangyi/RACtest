80d638e72b14d05ba09563c9016aad8bdc7aa4ea
Joe Yang <joe.yangyi@gmail.com>
2014-10-09 18:42:29 +0800

    Initial Commit

---
+++ RACtest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RACtest
+//
+//  Created by leaping on 10/9/14.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
